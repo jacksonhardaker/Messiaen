822259c32d88f9f76e50707cc513e6901474c67e
Jackson <jackson.hardaker@gmail.com>
2018-07-09 17:32:04 -0400

    Temporarily rename folder

--- src/js/messiaen.audio.js
+++ src/js/messiaen.audio.js
@@ -1,220 +1,217 @@
 // Messiaen Audio
-var messiaen = (messiaen) ? messiaen : {};
-messiaen.audio = function ($) {
 
-    var methods = {};
-    var _keyboard = [];
-    var _pitches = ['C', 'C#/Db', 'D', 'D#/Eb', 'E', 'F', 'F#/Gb', 'G', 'G#/Ab', 'A', 'Bb', 'B'];
-    var _folders = ['C', 'Db', 'D', 'Eb', 'E', 'F', 'Gb', 'G', 'Ab', 'A', 'Bb', 'B'];
-    var _notesToPlay = [];
-    var _maxChannels = 100;
-    var _audioChannels = [];
-    var _playingChord = false;
-    var _unisonNotesToPlay = [];
+const messiaenAudio = {};
+const _pitches = ['C', 'C#/Db', 'D', 'D#/Eb', 'E', 'F', 'F#/Gb', 'G', 'G#/Ab', 'A', 'Bb', 'B'];
+const _folders = ['C', 'Db', 'D', 'Eb', 'E', 'F', 'Gb', 'G', 'Ab', 'A', 'Bb', 'B'];
+const _maxChannels = 100;
+const _keyboard = [];
 
-    methods.initialise = function () {
-        //mp3 now support by all but old IE and Opera Mini, so we'll switch to this.
-        //var folder = (BrowserDetect.browser === "Opera" || BrowserDetect.browser === "Firefox") ? "Audio/Ogg/" : "Audio/Mp3/";
-        //var format = (BrowserDetect.browser === "Opera" || BrowserDetect.browser === "Firefox") ? ".ogg" : ".mp3";
-        var folder = "Audio/Mp3/";
-        var format = ".mp3";
+let _notesToPlay = [];
+let _audioChannels = [];
+let _playingChord = false;
+let _unisonNotesToPlay = [];
 
-        for (var j = 0; j < 5; j++) {
-            for (var k = 0; k < 12; k++) {
+messiaenAudio.initialise = function () {
+    var folder = "Audio/Mp3/";
+    var format = ".mp3";
 
-                _keyboard.push({ pitch: _pitches[k], audio: folder + _folders[k] + "/" + (j + 1) + format, id: _pitches[k] + (j + 1), loaded: false });
-            }
+    for (var j = 0; j < 5; j++) {
+        for (var k = 0; k < 12; k++) {
+
+            _keyboard.push({
+                pitch: _pitches[k],
+                audio: folder + _folders[k] + "/" + (j + 1) + format,
+                id: _pitches[k] + (j + 1),
+                loaded: false
+            });
         }
+    }
 
-        for (var i = 0; i < _maxChannels; i++) {
+    for (var i = 0; i < _maxChannels; i++) {
 
-            var audioObj = [];
-            audioObj.channel = new Audio();
-            audioObj.finished = -1;
-            _audioChannels.push(audioObj);
+        var audioObj = [];
+        audioObj.channel = new Audio();
+        audioObj.finished = -1;
+        _audioChannels.push(audioObj);
+    }
+};
+
+messiaenAudio.loadPitch = function (pitch) {
+
+    for (var i = 0; i < _keyboard.length; i++) {
+
+        var note = _keyboard[i];
+
+        if (note.pitch === pitch && !note.loaded) {
+
+            var audioTag = document.createElement("audio");
+            audioTag.setAttribute("preload", "auto");
+            audioTag.setAttribute("id", note.id);
+            audioTag.setAttribute("src", note.audio);
+            document.body.appendChild(audioTag);
+            _keyboard[i].loaded = true;
         }
-    };
+    }
+};
 
 
 
-    methods.loadPitch = function (pitch) {
+messiaenAudio.playChord = function (pitchArray) {
 
-        for (var i = 0; i < _keyboard.length; i++) {
+    if (pitchArray.length > 0) {
 
-            var note = _keyboard[i];
+        if (!_playingChord) {
 
-            if (note.pitch === pitch && !note.loaded) {
+            _playingChord = true;
+            var chord = [];
+            var keyboardStartingIndex = 0;
+            var noteAdded = false;
 
-                var audioTag = document.createElement("audio");
-                audioTag.setAttribute("preload", "auto");
-                audioTag.setAttribute("id", note.id);
-                audioTag.setAttribute("src", note.audio);
-                document.body.appendChild(audioTag);
-                _keyboard[i].loaded = true;
-            }
+            for (var i = 0; i < pitchArray.length; i++) {
 
-        }
+                var pitch = pitchArray[i];
+                noteAdded = false;
 
-    };
+                for (var j = keyboardStartingIndex; j < _keyboard.length; j++) {
 
-    var __play = function () {
+                    if (_keyboard[j].pitch === pitch) {
+                        if (_keyboard[j].loaded === false) {
+                            messiaen.audio.loadPitch(pitch);
+                        }
 
-        var audio = document.getElementById(_notesToPlay[0]);
+                        chord.push(_keyboard[j].id);
+                        keyboardStartingIndex = j;
+                        noteAdded = true;
 
-        for (var i = 0; i < _maxChannels; i++) {
+                        break;
+                    }
 
-            var now = new Date();
+                }
 
-            if (_audioChannels[i].finished < now.getTime()) {			// is this channel finished?
+                if (!noteAdded) {
 
-                _audioChannels[i].finished = now.getTime() + audio.duration * 1000;
-                _audioChannels[i].channel.src = audio.src;
-                _audioChannels[i].channel.load();
-                _audioChannels[i].channel.play();
+                    for (var jj = keyboardStartingIndex; jj >= 0; jj--) {
 
-                break;
-            }
-        }
-
-        _unisonNotesToPlay.push(_notesToPlay[0]);
-        _notesToPlay.splice(0, 1);
-
-        if (_notesToPlay.length > 0) {
-            setTimeout(__play, 150);
-        }
-        else {
-            setTimeout(__playUnisonChord, 900);
-        }
-    };
-
-
-
-    var __playUnisonChord = function () {
-
-        var audio = document.getElementById(_unisonNotesToPlay[0]);
-
-        for (var i = 0; i < _maxChannels; i++) {
-
-            var now = new Date();
-
-            if (_audioChannels[i].finished < now.getTime()) {			// is this channel finished?
-
-                _audioChannels[i].finished = now.getTime() + audio.duration * 1000;
-                _audioChannels[i].channel.src = audio.src;
-                _audioChannels[i].channel.load();
-                _audioChannels[i].channel.play();
-
-                break;
-            }
-
-        }
-
-        _unisonNotesToPlay.splice(0, 1);
-
-        if (_unisonNotesToPlay.length > 0) {
-            __playUnisonChord();
-        }
-        else {
-            _playingChord = false;
-            $($("#playMessiaenChordButton").children()[0]).css("display", "block");
-            $($("#playMessiaenChordButton").children()[1]).css("display", "none");
-        }
-    };
-
-
-
-    methods.playChord = function (pitchArray) {
-
-        if (pitchArray.length > 0) {
-
-            if (!_playingChord) {
-
-                _playingChord = true;
-                var chord = [];
-                var keyboardStartingIndex = 0;
-                var noteAdded = false;
-
-                for (var i = 0; i < pitchArray.length; i++) {
-
-                    var pitch = pitchArray[i];
-                    noteAdded = false;
-
-                    for (var j = keyboardStartingIndex; j < _keyboard.length; j++) {
-
-                        if (_keyboard[j].pitch === pitch) {
-                            if (_keyboard[j].loaded === false) {
+                        if (_keyboard[jj].pitch === pitch) {
+                            if (_keyboard[jj].loaded === false) {
                                 messiaen.audio.loadPitch(pitch);
                             }
 
-                            chord.push(_keyboard[j].id);
-                            keyboardStartingIndex = j;
+                            chord.push(_keyboard[jj].id);
+                            keyboardStartingIndex = jj;
                             noteAdded = true;
 
                             break;
                         }
-
-                    }
-
-                    if (!noteAdded) {
-
-                        for (var jj = keyboardStartingIndex; jj >= 0; jj--) {
-
-                            if (_keyboard[jj].pitch === pitch) {
-                                if (_keyboard[jj].loaded === false) {
-                                    messiaen.audio.loadPitch(pitch);
-                                }
-
-                                chord.push(_keyboard[jj].id);
-                                keyboardStartingIndex = jj;
-                                noteAdded = true;
-
-                                break;
-                            }
-                        }
                     }
                 }
+            }
 
-                __playChord(chord);
+            __playChord(chord);
+        }
+    }
+};
+
+/**
+ * Private Functions
+ */
+
+function __play() {
+
+    var audio = document.getElementById(_notesToPlay[0]);
+
+    for (var i = 0; i < _maxChannels; i++) {
+
+        var now = new Date();
+
+        if (_audioChannels[i].finished < now.getTime()) {			// is this channel finished?
+
+            _audioChannels[i].finished = now.getTime() + audio.duration * 1000;
+            _audioChannels[i].channel.src = audio.src;
+            _audioChannels[i].channel.load();
+            _audioChannels[i].channel.play();
+
+            break;
+        }
+    }
+
+    _unisonNotesToPlay.push(_notesToPlay[0]);
+    _notesToPlay.splice(0, 1);
+
+    if (_notesToPlay.length > 0) {
+        setTimeout(__play, 150);
+    }
+    else {
+        setTimeout(__playUnisonChord, 900);
+    }
+};
+
+
+
+function __playUnisonChord() {
+
+    var audio = document.getElementById(_unisonNotesToPlay[0]);
+
+    for (var i = 0; i < _maxChannels; i++) {
+
+        var now = new Date();
+
+        if (_audioChannels[i].finished < now.getTime()) {			// is this channel finished?
+
+            _audioChannels[i].finished = now.getTime() + audio.duration * 1000;
+            _audioChannels[i].channel.src = audio.src;
+            _audioChannels[i].channel.load();
+            _audioChannels[i].channel.play();
+
+            break;
+        }
+
+    }
+
+    _unisonNotesToPlay.splice(0, 1);
+
+    if (_unisonNotesToPlay.length > 0) {
+        __playUnisonChord();
+    }
+    else {
+        _playingChord = false;
+        document.getElementById('playMessiaenChordButton').children[0].style.display = 'block';
+        document.getElementById('playMessiaenChordButton').children[1].style.display = 'none';
+    }
+};
+
+function __playChord(chord) {
+
+    if (chord && chord.length !== 0) {
+        document.getElementById('playMessiaenChordButton').children[0].style.display = 'none';
+        document.getElementById('playMessiaenChordButton').children[1].style.display = 'block';
+
+        var allLoaded = true;
+
+        for (var i = 0, length = chord.length; i < length; i++) {
+            if (!document.getElementById(chord[i]).readyState) {
+                allLoaded = false;
+
+                break;
             }
         }
-    };
 
-
-
-    var __playChord = function (chord) {
-
-        if (chord && chord.length !== 0) {
-            $($("#playMessiaenChordButton").children()[0]).css("display", "none");
-            $($("#playMessiaenChordButton").children()[1]).css("display", "block");
-
-            var allLoaded = true;
-
-            for (var i = 0, length = chord.length; i < length; i++) {
-                if (!document.getElementById(chord[i]).readyState) {
-                    allLoaded = false;
-
-                    break;
-                }
+        if (allLoaded) {
+            for (var k = 0; k < chord.length; k++) {
+                _notesToPlay.push(chord[k]);
             }
 
-            if (allLoaded) {
-                for (var k = 0; k < chord.length; k++) {
-                    _notesToPlay.push(chord[k]);
-                }
-
-                __play();
-            }
-            else {
-                // Wait a wee bit for the sounds to load, then try playing again.
-                setTimeout(function () { __playChord(chord); }, 100);
-            }
+            __play();
         }
         else {
-            $($("#playMessiaenChordButton").children()[0]).css("display", "block");
-            $($("#playMessiaenChordButton").children()[1]).css("display", "none");
+            // Wait a wee bit for the sounds to load, then try playing again.
+            setTimeout(function () { __playChord(chord); }, 100);
         }
-    };
+    }
+    else {
+        document.getElementById('playMessiaenChordButton').children[0].style.display = 'block';
+        document.getElementById('playMessiaenChordButton').children[1].style.display = 'none';
+    }
+};
 
-    return methods;
-
-}(jQuery);
+export default messiaenAudio;
