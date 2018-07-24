bc8d8718805390e6d7f5ab8d7ebdee8504971647
Jackson <jackson.hardaker@gmail.com>
2018-07-23 22:06:04 -0400

    Update urls

--- src/app.js
+++ src/app.js
@@ -8,7 +8,8 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'font-awesome/css/font-awesome.min.css';
 
-import '../images/messiaen.png';
+// import '../images/messiaen.png';
+// import '../images/43220697cd26145788f3b4171a2.jpg'
 
 //scss
 import './scss/typography.scss';
--- webpack.config.js
+++ webpack.config.js
@@ -1,49 +1,56 @@
-const path = require('path');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
-const webpack = require('webpack');
+const path = require("path");
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const webpack = require("webpack");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
 
 module.exports = {
-    mode: 'development',
-    entry: './src/app.js',
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.js',
-        publicPath: '/messiaen/',
-        hotUpdateChunkFilename: 'hot/hot-update.js',
-        hotUpdateMainFilename: 'hot/hot-update.json'
-    },
-    module: {
-        rules: [
-            {
-                test: /\.scss$/,
-                use: ['css-hot-loader'].concat(ExtractTextPlugin.extract({
-                    fallback: 'style-loader',
-                    use: ['css-loader', 'resolve-url-loader', 'sass-loader?sourceMap']
-                }))
-            },
-            {
-                test: /\.css$/,
-                use: ['css-hot-loader'].concat(ExtractTextPlugin.extract({
-                    fallback: 'style-loader',
-                    use: ['css-loader']
-                }))
-            },
-            {
-                test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-                loader: "url-loader?limit=10000&mimetype=application/font-woff"
-            },
-            {
-                test: /\.(ttf|eot|svg|png|jpg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-                loader: "file-loader"
-            }
-        ]
-    },
-    plugins: [
-        new ExtractTextPlugin('style.css')
-    ],
-    devServer: {
-        contentBase: __dirname,
-        compress: true,
-        port: 9000
-    }
+  mode: "development",
+  entry: "./src/app.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+    publicPath: "/messiaen/",
+    hotUpdateChunkFilename: "hot/hot-update.js",
+    hotUpdateMainFilename: "hot/hot-update.json"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: ["css-hot-loader"].concat(
+          ExtractTextPlugin.extract({
+            publicPath: "/messiaen/dist/",
+            fallback: "style-loader",
+            use: ["css-loader", "resolve-url-loader", "sass-loader?sourceMap"]
+          })
+        )
+      },
+      {
+        test: /\.css$/,
+        use: ["css-hot-loader"].concat(
+          ExtractTextPlugin.extract({
+            publicPath: "/messiaen/dist/",
+            fallback: "style-loader",
+            use: ["css-loader"]
+          })
+        )
+      },
+      {
+        test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        loader: "url-loader?limit=10000&mimetype=application/font-woff"
+      },
+      {
+        test: /\.(ttf|eot|svg|png|jpg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        loader: "file-loader"
+      }
+    ]
+  },
+  plugins: [
+    new ExtractTextPlugin("style.css")
+  ],
+  devServer: {
+    contentBase: __dirname,
+    compress: true,
+    port: 9000
+  }
 };
